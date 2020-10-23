74baf9efb7dff796a2bc94fb72c755c5723217bb
Ramya.U <ramyayadav2016@gmail.com>
2020-10-23 23:18:57 +0530

    Added UC-7

--- Day04-Snake and Ladder/Program.cs
+++ Day04-Snake and Ladder/Program.cs
@@ -15,23 +15,58 @@ namespace Day04_Snake_and_Ladder
 {
     class Program
     {
-        //constants
-        public const int START_POINT = 0;
-        public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
-            //user input
-            string player1 = Console.ReadLine();
-            //initialization
-            int diceRoll = DiceRoll();
-            Console.WriteLine("Dice Roll : " + diceRoll);
-        }
-        static int DiceRoll()
-        {
-            Random random = new Random();
-            int diceNumber = random.Next(1, 7);
-            return diceNumber;
+            //constants
+            const int noPlayOption = 0;
+            const int ladderOption = 1;
+            const int snakeOption = 2;
+            int player = 0;
+            //2 players are starting the game
+            int[] playerPosition = new int[2] { 0, 0 }; 
+            int diceRollCount = 0;
+            //while loop to check which player is playing at which position
+            while (playerPosition[player] < 100)
+            {
+                Random random = new Random();
+                int dieRoll = random.Next(1, 7);
+                diceRollCount++;
+                int options = random.Next(0, 3);
+                //switch case to check the condition
+                switch (options)
+                {
+                    case noPlayOption:
+                        Console.WriteLine("Player " + player + "Position: " + playerPosition[player]);
+                        break;
+                    case ladderOption:
+                        if (playerPosition[player] + dieRoll <= 100)
+                        {
+                            playerPosition[player] = playerPosition[player] + dieRoll;
+                        }
+                        Console.WriteLine("Player " + player + "Position: " + playerPosition[player]);
+                        break;
+                    case snakeOption:
+                        playerPosition[player] = playerPosition[player] - dieRoll;
+                        if (playerPosition[player] < 0)
+                        {
+                            playerPosition[player] = 0;
+                        }
+                        Console.WriteLine("Player " + player + " Position: " + playerPosition[player]);
+                        break;
+                }
+                if (options == noPlayOption || options == snakeOption)
+                {
+                    if (player == 0)
+                    {
+                        player = 1;
+                    }
+                    else
+                    {
+                        player = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Player " + player + " Wins with Die Roll :" + diceRollCount);
         }
     }
 }
