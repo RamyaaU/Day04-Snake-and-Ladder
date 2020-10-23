f4626310ad45b0ee871a626a6f2e9e79b7183144
Ramya.U <ramyayadav2016@gmail.com>
2020-10-23 22:41:18 +0530

    Added UC-3

--- Day04-Snake and Ladder/Program.cs
+++ Day04-Snake and Ladder/Program.cs
@@ -1,8 +1,8 @@
 ﻿/******************************************************************************
- *  Compilation:  csc.exe /t:exe /out:Day04EmployeeWageCompuatation.exe Day03EmployeeWageCompuatation.cs
+ *  Compilation:  csc.exe /t:exe /out:Day04Snake and Ladder
  *  Execution:    Day04SNAKE AND LADDER.exe 103
  *  
- *  Purpose: Player rolls the die
+ *  Purpose: Player checks option
  *
  *  @author  Ramya U
  *  @version 1.0
@@ -17,21 +17,46 @@ namespace Day04_Snake_and_Ladder
     {
         //constants
         public const int START_POINT = 0;
-        public const int END_POINT = 100;
+        public const int FINISH_POINT = 10;
+        public const int NO_PLAY = 0;
+        public const int SNAKE = 1;
+        public const int LADDER = 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
-            //user input
-            string player1 = Console.ReadLine();
-            //initialization
+            string player1 = Console.ReadLine();    //userinput
+            int playerPosition = START_POINT;  //initialization
             int diceRoll = DiceRoll();
             Console.WriteLine("Dice Roll : " + diceRoll);
+            playerPosition = PlayerMovement(diceRoll, playerPosition); 
+            Console.WriteLine("Your Position: " + playerPosition);
         }
         static int DiceRoll()
         {
             Random random = new Random();
-            int diceNumber = random.Next(1, 7);
+            int diceNumber = random.Next(1, 7); //returns a number between 1 to 7
             return diceNumber;
+        }
+        static int PlayerMovement(int numberRolled, int playerPosition)
+        {
+            Random random = new Random();
+            int move = random.Next(0, 3);
+            switch (move) //using switch case to check the conditions
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case SNAKE:
+                    Console.WriteLine("Snake");
+                    playerPosition = playerPosition - numberRolled;
+                    break;
+                case LADDER:
+                    Console.WriteLine("Ladder");
+                    playerPosition = playerPosition + numberRolled;
+                    break;
+            }
+            return playerPosition;
         }
     }
 }
+
