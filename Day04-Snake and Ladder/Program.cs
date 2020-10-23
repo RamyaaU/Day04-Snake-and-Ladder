dd59f348502c7853d9edf869192a2eb375451e89
Ramya.U <ramyayadav2016@gmail.com>
2020-10-23 22:27:32 +0530

    Added UC-1

--- Day04-Snake and Ladder/Program.cs
+++ Day04-Snake and Ladder/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿/******************************************************************************
+ *  Compilation:  csc.exe /t:exe /out:Day04EmployeeWageCompuatation.exe Day03EmployeeWageCompuatation.cs
+ *  Execution:    Day04SNAKE AND LADDER.exe 103
+ *  
+ *  Purpose: Game played with single player
+ *
+ *  @author  Ramya U
+ *  @version 1.0
+ *  @since   22-10-2020
+ *
+ ******************************************************************************/
 
-namespace Day04_Snake_and_Ladder
+using System;
+
+namespace SnakeAndLadder
 {
     class Program
     {
+        //constants
+        public const int START_POINT = 0;
+        public const int FINISH_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder!");
+            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
+            string player1 = Console.ReadLine(); //userinput
+            int playerPosition = START_POINT;
+            Console.WriteLine("Current Position : " + playerPosition);
         }
     }
 }
