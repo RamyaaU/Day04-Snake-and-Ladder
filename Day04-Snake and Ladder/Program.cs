7543138dbe5001452be5805048708725bb00e4a5
Ramya.U <ramyayadav2016@gmail.com>
2020-10-23 23:10:37 +0530

    Added UC-4

--- Day04-Snake and Ladder/Program.cs
+++ Day04-Snake and Ladder/Program.cs
@@ -15,69 +15,38 @@ namespace Day04_Snake_and_Ladder
 {
     class Program
     {
-        //constants
-        public const int START_POINT = 0;
-        public const int FINISH_POINT = 100;
-        public const int NO_PLAY = 0;
-        public const int SNAKE = 1;
-        public const int LADDER = 2;
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
-            string player1 = Console.ReadLine(); //userinput
-            int playerCurrentPosition = START_POINT; //initialization
-            //for loop is used to check no of times dice rolled and current position of player
-            //and also executes repeatedly until the condition fails
-            for (int noOfTimesDiceRolled = 1; playerCurrentPosition >= 0; noOfTimesDiceRolled++)
+            //constants
+            const int noPlayOption = 0;
+            const int ladderOption = 1;
+            const int snakeOption = 2;
+            const int playerOne = 1;
+            //variable
+            int positionOne = 0;
+            while (positionOne < 100)
             {
-                int diceRoll = DiceRoll();
-                Console.WriteLine("You rolled: " + diceRoll);
-                playerCurrentPosition = PlayerMovement(diceRoll, playerCurrentPosition);
-                Console.WriteLine("Your position: " + playerCurrentPosition);
-                if (playerCurrentPosition >= 100)
+                Random random = new Random();
+                int dieRoll = random.Next(1, 7);
+                int options = random.Next(0, 3);
+                switch (options)
                 {
-                    Console.WriteLine("Game Over");
-                    break;
+                    case noPlayOption:
+                        positionOne = positionOne + 0;
+                        break;
+                    case ladderOption:
+                        positionOne = positionOne + dieRoll;
+                        break;
+                    case snakeOption:
+                        positionOne = positionOne - dieRoll;
+                        if (positionOne < 0)
+                        {
+                            positionOne = 0;
+                        }
+                        break;
                 }
-                Console.ReadLine();
             }
-        }
-        static int DiceRoll()
-        {
-            Random random = new Random();
-            int diceNumber = random.Next(1, 7); //returns a number between 1 and 7
-            return diceNumber;
-        }
-
-        static int PlayerMovement(int numberRolled, int playerPosition)
-        {
-            Random random = new Random();
-            int move = random.Next(0, 3);
-            //using switch case to check the conditions
-            switch (move)
-            {
-                case NO_PLAY:
-                    Console.WriteLine("No Play");
-                    break;
-                case SNAKE:
-                    Console.WriteLine("Snake");
-                    if (playerPosition - numberRolled >= 0)
-                    {
-                        playerPosition = playerPosition - numberRolled;
-                        break;
-                    }
-                    else
-                    {
-                        playerPosition = START_POINT;
-                        break;
-                    }
-                case LADDER:
-                    Console.WriteLine("Ladder");
-                    playerPosition = playerPosition + numberRolled;
-                    break;
-            }
-            return playerPosition;
+            Console.WriteLine("Player Wins with Position :" + positionOne);
         }
     }
 }
