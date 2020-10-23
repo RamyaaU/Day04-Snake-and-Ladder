431f6aa200a27ef17693912e45ac1851bdde0a5e
Ramya.U <ramyayadav2016@gmail.com>
2020-10-23 23:14:23 +0530

    Added UC-6

--- Day04-Snake and Ladder/Program.cs
+++ Day04-Snake and Ladder/Program.cs
@@ -15,23 +15,48 @@ namespace Day04_Snake_and_Ladder
 {
     class Program
     {
-        //constants
-        public const int START_POINT = 0;
-        public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
-            //user input
-            string player1 = Console.ReadLine();
-            //initialization
-            int diceRoll = DiceRoll();
-            Console.WriteLine("Dice Roll : " + diceRoll);
-        }
-        static int DiceRoll()
-        {
-            Random random = new Random();
-            int diceNumber = random.Next(1, 7);
-            return diceNumber;
+            //constants
+            const int noPlayOption = 0;
+            const int ladderOption = 1;
+            const int snakeOption = 2;
+            const int playerOne = 1;
+            //variable
+            int positionOne = 0;
+            int diceRollCount = 0;
+            //while loop to check the condition of position is greater than zero
+            while (positionOne < 100)
+            {
+                Random random = new Random();
+                int dieRoll = random.Next(1, 7);
+                diceRollCount++;
+                int options = random.Next(0, 3);
+                Console.WriteLine("Position: " + positionOne);
+                switch (options)
+                {
+                    case noPlayOption:
+                        Console.WriteLine("Position: " + positionOne);
+                        break;
+                    case ladderOption:
+                        if (positionOne + dieRoll <= 100)
+                        {
+                            positionOne = positionOne + dieRoll;
+                        }
+                        Console.WriteLine("Position: " + positionOne);
+                        break;
+                    case snakeOption:
+                        positionOne = positionOne - dieRoll;
+                        if (positionOne < 0)
+                        {
+                            positionOne = 0;
+                        }
+                        Console.WriteLine("Position: " + positionOne);
+                        break;
+                }
+            }
+            Console.WriteLine("Player Wins with Die Roll :" + diceRollCount);
         }
     }
 }
+
