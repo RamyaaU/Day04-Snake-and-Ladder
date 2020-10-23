2c4cb419e57a9aa9bf82ad2a609af3c1ad8cfd0c
Ramya.U <ramyayadav2016@gmail.com>
2020-10-23 22:33:50 +0530

    Added UC-2

--- Day04-Snake and Ladder/Program.cs
+++ Day04-Snake and Ladder/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿/******************************************************************************
+ *  Compilation:  csc.exe /t:exe /out:Day04EmployeeWageCompuatation.exe Day03EmployeeWageCompuatation.cs
+ *  Execution:    Day04SNAKE AND LADDER.exe 103
+ *  
+ *  Purpose: Player rolls the die
+ *
+ *  @author  Ramya U
+ *  @version 1.0
+ *  @since   22-10-2020
+ *
+ ******************************************************************************/
+using System;
 
 namespace Day04_Snake_and_Ladder
 {
     class Program
     {
+        //constants
+        public const int START_POINT = 0;
+        public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder!");
+            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
+            //user input
+            string player1 = Console.ReadLine();
+            //initialization
+            int diceRoll = DiceRoll();
+            Console.WriteLine("Dice Roll : " + diceRoll);
+        }
+        static int DiceRoll()
+        {
+            Random random = new Random();
+            int diceNumber = random.Next(1, 7);
+            return diceNumber;
         }
     }
 }
